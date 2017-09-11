34ed766e8d473d0357815a3e7a66e33ca9f24076
Lukas Mohs <ga47boq@mytum.de>
2017-09-11 23:12:43 +0200

    Initial Commit

---
+++ ImageFetcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageFetcher
+//
+//  Created by Lukas Mohs on 11.09.17.
+//  Copyright © 2017 Lukas Mohs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
